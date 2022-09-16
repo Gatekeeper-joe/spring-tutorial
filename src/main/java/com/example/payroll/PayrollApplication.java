6e728aa48c9674663663ac53f4981826ab15ed12
Gatekeeper-joe <investorjk5x@gmail.com>
2022-09-16 10:46:10 +0900

    コメント削除

--- src/main/java/com/example/payroll/PayrollApplication.java
+++ src/main/java/com/example/payroll/PayrollApplication.java
@@ -3,16 +3,8 @@ package com.example.payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 給与計算アプリのメインクラス
- */
 @SpringBootApplication
 public class PayrollApplication {
-
-	/**
-	 * mainメソッド
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
