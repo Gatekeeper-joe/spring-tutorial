9e9e2d2bc280487d1d367da0f6fc911f38ba997e
Gatekeeper-joe <investorjk5x@gmail.com>
2022-09-21 17:09:39 +0900

    OrderRepositoryクラスに関する記述を追加

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -16,16 +16,27 @@ class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     /**
-     * 従業員情報をログに出力
-     * @param repository 従業員リポジトリ
+     * リポジトリに登録した情報をログに出力
+     * @param employeeRepository 従業員リポジトリ
+     * @param orderRepository 注文リポジトリ
      * @return String
      */
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("梶原", "平社員")));
-            log.info("Preloading " + repository.save(new Employee("梶原", "平社員")));
+            employeeRepository.save(new Employee("梶原", "丈一朗", "平社員"));
+            employeeRepository.save(new Employee("大谷", "翔平", "部長"));
+
+            employeeRepository.findAll().forEach(
+                employee -> log.info("Preloaded " + employee)
+            );
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> log.info("Preloaded " + order));
         };
     }
 }
+
