393834bfaea93e691e4f20859c395054abf34f78
Gatekeeper-joe <investorjk5x@gmail.com>
2022-09-26 13:56:13 +0900

    public追加

--- src/main/java/payroll/entity/Employee.java
+++ src/main/java/payroll/entity/Employee.java
@@ -36,7 +36,7 @@ public class Employee {
     /**
      * コンストラクタ
      */
-    Employee() {}
+    public Employee() {}
 
     /**
      * コンストラクタ
--- src/main/java/payroll/entity/Order.java
+++ src/main/java/payroll/entity/Order.java
@@ -34,7 +34,7 @@ public class Order {
     /**
      * コンストラクタ
      */
-    Order() {}
+    public Order() {}
 
     /**
      * コンストラクタ
